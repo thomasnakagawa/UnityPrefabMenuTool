c8e7bb316c9a368495fb0c60ede05bf87f06d33f
Thomas Nakagawa <thomasnakagawa@gmail.com>
2019-12-28 21:46:57 -0800

    Add ability to instantiate prefabs from the menuitems

--- Assets/Editor/PrefabMenuTool/PrefabMenuUtils.cs
+++ Assets/Editor/PrefabMenuTool/PrefabMenuUtils.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
 
 namespace float_oat.PrefabMenuTool
@@ -10,11 +11,42 @@ namespace float_oat.PrefabMenuTool
     {
         public static void InstantiateInScene(string prefabPath)
         {
-            // validate argument
-            // check that prefabpath leads to a valid prefab
-            // instantiate the prefab at the focus position
-            // parent the instantiated prefab to the selected object if there is one
-            // register Undo
+            try
+            {
+                // validate argument
+                if (string.IsNullOrEmpty(prefabPath))
+                {
+                    throw new ArgumentException("Prefab path cannot be empty");
+                }
+
+                // check that prefabpath leads to a valid prefab
+                GameObject prefabObject = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+                if (prefabObject == null)
+                {
+                    throw new System.IO.FileNotFoundException("Could not find prefab at " + prefabPath + ". The prefab file may have been moved, renamed or deleted.");
+                }
+
+                // instantiate the prefab at the focus position
+                GameObject prefabInstance = (GameObject)PrefabUtility.InstantiatePrefab(prefabObject);
+
+                // parent the instantiated prefab to the selected object if there is one
+                if (Selection.activeTransform != null)
+                {
+                    prefabInstance.transform.parent = Selection.activeTransform;
+                }
+
+                // select the new object
+                Selection.activeGameObject = prefabInstance;
+
+                // register Undo
+                Undo.RegisterCreatedObjectUndo(prefabInstance, "Created " + prefabInstance.name);
+
+            }
+            catch (Exception e)
+            {
+                EditorUtility.DisplayDialog("Could not instantiate prefab", e.Message, "Ok");
+                throw e;
+            }
         }
     }
 }
